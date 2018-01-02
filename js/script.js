08ad51e50bea63a92083adc688c9b4b297856ee8
Alex Kim <askim1019@gmail.com>
2018-01-02 21:51:43 +0000

    add second scrolldown btn

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-    $('#scrolldown-btn').click(function(){
+    $('#scrolldown-btn1').click(function(){
        $('html, body').animate({
         scrollTop: $("#history").offset().top
         }, 1000);
     });
+    
+    $('#scrolldown-btn2').click(function(){
+       $('html, body').animate({
+           scrollTop: $('#servicediv').offset().top
+        }, 1000);
+    });
+    
 });
